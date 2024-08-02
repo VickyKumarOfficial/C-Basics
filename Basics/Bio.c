ceadb214dc2583ac903a31f352fe0343b9a8605b
VickyKumarOfficial <vickykofficial890@gmail.com>
2024-08-02 20:34:01 +0530

    lecture

---
+++ Basics/Bio.c
@@ -0,0 +1,13 @@
+// Print your details in new lines:
+
+#include <stdio.h>
+
+int main(){
+    printf("Father/Mother's name : Dharmendra Kumar\n");
+    printf("Hobbies : Playing Football\n");
+    printf("Contact number : 8875660473\n");
+    printf("Favourite actor : Sharukh Khan\n");
+    printf("Favourite Sport : Football\n");
+
+    return 0;
+}
--- Loops/Questions/SumofReverse.c
+++ Loops/Questions/SumofReverse.c
@@ -1,23 +1,41 @@
 // Q. WAP to print the sum of given number and it's reverse.
 
+// #include<stdio.h>
+
+// int main(){
+//     int n, sum, Revno = 0, remainder;
+//     printf("Enter a number :");
+//     scanf("%d",&n);
+//     while (n>0) {
+//             remainder = n % 10;
+//             Revno = Revno * 10 + remainder;
+//             n = n / 10;
+//     }
+
+//     printf("Reversed number is :%d\n",Revno);
+//     return 0;
+// }
+
+
+// UNDER DEVELOPMENT :(
+
+
 #include<stdio.h>
 
 int main(){
-    int n, sum, Revno = 0, remainder;
+    int n, r, d1, d2, temp; 
+    // n -> input from user Ex - 1234
+    // r -> store last digits from n by taking by n%10
+    // d1 ->
+    // d2 ->
+    // temp ->
     printf("Enter a number :");
     scanf("%d",&n);
-    while (n) {
-            remainder = n % 10;
-            Revno = Revno * 10 + remainder;
-            n = n / 10;
+    while (n)
+    {
+        n = n % 10;
+        
     }
-    // printf("Reversed number is :%d\n",Revno);
-    // printf("and initial given number is :%d\n",n);
-    // sum = Revno + n;
-    // printf("The total sum of both the number is :%d",sum);
-    sum = n / 10;
-    int original;
-    original = n;
-    printf("Reversed number is :%d\n");
+    
     return 0;
 }
