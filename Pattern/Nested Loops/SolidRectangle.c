8ada123d0b8e390d45fa71e35c2a984d2e29e9af
VickyKumarOfficial <vickykofficial890@gmail.com>
2024-08-12 09:09:47 +0530

    1st hour

--- Pattern/Nested Loops/SolidRectangle.c
+++ Pattern/Nested Loops/SolidRectangle.c
@@ -12,4 +12,3 @@ int main(){
 
     return 0;
 }
-// Q. Write a C program to test whether Ravi has won the game or not (The game is having two levels if he clears level 1 then he is allowed to play level 2. To clear level 1, score should be more than 70, to clear level 2, score should be more than 80).
---
+++ if/Nested If/Basics.c
@@ -0,0 +1,21 @@
+// Q. Write a C program to test whether Ravi has won the game or not (The game is having two levels if he clears level 1 then he is allowed to play level 2. To clear level 1, score should be more than 70, to clear level 2, score should be more than 80).
+// #include<stdio.h>
+
+// int main(){
+//     int L1;
+//     printf("Enter lvl 1 score :");
+//     scanf("%d",&L1);
+//     if (L1>=70){
+//         Printf("Hurraayyy!!! you passed level 1 and can proceed to play level 2");
+//         int L2;
+//         printf("Enter lvl 2 score :");
+//         scanf("%d",&L2);
+//         if (L2>=80){
+//             printf("Awesome! You passed level 2 and can go to level 3");
+//         }
+//         else printf("You can not go to lvl 3");    
+//     }
+//     else printf("You can not go to lvl 2");
+//     return 0;
+// }
+// Q. Write C program to test whether the given number is +ve or not. If it is +ve then find it's square & if the square is even then display even square otherwise display odd square. If the number is -ve find it's cube.
