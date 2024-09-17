d8e4c69e769ffd666541514734315c324645322d
VickyKumarOfficial <vickykofficial890@gmail.com>
2024-09-17 19:53:59 +0530

    File anmes

--- Pattern/Nested Loops/NumberSquare.c
+++ Pattern/Nested Loops/NumberSquare.c
@@ -48,49 +48,24 @@
 //      3333
 //      2222
 //      1111
-// #include<stdio.h>
-
-// int main(){
-//     int n;
-//     printf("Enter a number :");
-//     scanf("%d",&n);
-//     for (int i = n; i >= 1; i--)
-//     {
-//         for (int j = 1; j <= n; j++){
-            
-//             printf("%d ",i);
-//         }
-//         printf("\n");
-//     }
-    
-//     return 0;
-// }
-
-
-// iv.  1  2  3  4 
-//      5  6  7  8
-//      9 10 11 12
-//     13 14 15 16
-
 #include<stdio.h>
 
 int main(){
     int n;
     printf("Enter a number :");
     scanf("%d",&n);
-    int x = n*n;
-    for (int i = 1; i <= n; i++)
+    for (int i = n; i >= 1; i--)
     {
         for (int j = 1; j <= n; j++){
             
-            printf("%3d ",x); // %3d is used to display the pattern in an structred way.
-            x--;
+            printf("%d ",i);
         }
         printf("\n");
     }
     
     return 0;
 }
+
 
 // v.  13 14 15 16  
 //     9  10 11 12
--- Pattern/Nested Loops/Squares/NumberIncreamentSquare.c
+++ Pattern/Nested Loops/Squares/NumberIncreamentSquare.c
@@ -1,4 +1,4 @@
-// WAP to display the following paatern.
+// WAP to display the following pattern.
 // iv.  1  2  3  4 
 //      5  6  7  8
 //      9 10 11 12
@@ -10,7 +10,7 @@ int main(){
     int n;
     printf("Enter a number :");
     scanf("%d",&n);
-    int x = n*n;
+    int x = n*n;    
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++){
