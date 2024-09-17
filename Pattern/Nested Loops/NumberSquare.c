c91718b0723e9a8d1d14cfd04434db7645fea6f8
VickyKumarOfficial <vickykofficial890@gmail.com>
2024-09-17 20:17:47 +0530

    Final folder change of the day

--- Pattern/Nested Loops/NumberSquare.c
+++ Pattern/Nested Loops/NumberSquare.c
@@ -1,26 +1,3 @@
-// Q. WAP to print pattern of Number Square by taking number as input form the user.
-// i. 1 1 1
-//    2 2 2
-//    3 3 3
-
-// #include<stdio.h>
-
-// int main(){
-//     int n;
-//     printf("Enter a number :");
-//     scanf("%d",&n);
-//     for (int i = 1; i <= n; i++)
-//     {
-//         for (int j = 1; j <= n; j++){
-            
-//             printf("%d ",i);
-//         }
-//         printf("\n");
-//     }
-    
-//     return 0;
-// }
-
 // v.  13 14 15 16  
 //     9  10 11 12
 //     5   6  7  8
