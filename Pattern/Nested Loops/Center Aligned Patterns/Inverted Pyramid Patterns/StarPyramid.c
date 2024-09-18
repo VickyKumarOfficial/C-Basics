a12fbc6600ada65093c47b8c126a96ff3df637e1
VickyKumarOfficial <vickykofficial890@gmail.com>
2024-09-18 09:30:40 +0530

    Questions

---
+++ Pattern/Nested Loops/Center Aligned Patterns/Inverted Pyramid Patterns/StarPyramid.c
@@ -0,0 +1,24 @@
+// Display the following pattern:
+//  * * * * * * *
+//   * * * * *
+//     * * *
+//       *
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    for (int i = n; i >= 1; i--) {
+       
+        for (int j = 1; j <= n-1; j++){
+        printf(" ");
+    }
+        for (int k = 1; k <= 2*i-1; k++){
+        printf("*");
+    }
+        printf("\n");
+    }
+    
+    return 0;
+}
