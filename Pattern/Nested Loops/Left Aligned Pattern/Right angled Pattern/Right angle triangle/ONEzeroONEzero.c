b99151eb0d92f18cce1b9dd64bfc6d6d485e9bdf
VickyKumarOfficial <vickykofficial890@gmail.com>
2024-10-02 10:34:14 +0530

    Functions started

--- Pattern/Nested Loops/Left Aligned Pattern/Right angled Pattern/Right angle triangle/ONEzeroONEzero.c
+++ Pattern/Nested Loops/Left Aligned Pattern/Right angled Pattern/Right angle triangle/ONEzeroONEzero.c
@@ -9,18 +9,43 @@
 #include<stdio.h>
 
 int main(){
-    int n;
+    int n, a=0;
     printf("Enter a number :");
     scanf("%d",&n);
     for(int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++){
-            if(j%2==0){
-                printf("1");
+        if(i%2) a = 1;
+        else a = 0;
+            for(int j = 1; j <= i; j++){
+                printf("%d ",a);
+                if(a==0) a = 1 ;
+                else a = 0 ;
             }
-            else printf("0");
-        }
-        
         printf("\n");
     }
     return 0;
 }
+
+
+
+/*
+    0
+    0 1
+    0 1 0
+    0 1 0 1
+*/
+
+// #include<stdio.h>
+
+// int main(){
+//     int n;
+//     printf("Enter a number :");
+//     scanf("%d",&n);
+//     for(int i = 1; i <= n; i++) {
+//         for (int j = 1; j <= i; j++){
+//             if(j%2==0) printf("1");
+//                 else printf("0");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
