1fb771a2d405d63eab5113121e23403f0005b6ec
VickyKumarOfficial <vickykofficial890@gmail.com>
2024-10-21 23:03:42 +0530

    Pass array to function

--- Arrays/1-D Arrays/PassArrayToFunc.c
+++ Arrays/1-D Arrays/PassArrayToFunc.c
@@ -1,3 +1,5 @@
+// Passing arrays to functions.
+
 #include<stdio.h>
 void fun(int a[]){
     a[0] = 34;
@@ -8,6 +10,43 @@ int main(){
     int a[5] = {1, 2, 4, 5, 6};
     printf("%d\n",a[0]);
     fun(a);
-    printf("%d",a[0]);
+    printf("%d",a[0]);  // If know the pointers concept than it is easy to understand
+                        // that we are passing by reference and we are not using pointers
+                        // to change the value in same address hence, value will be changed
+                        // if pointers were used then values will not be changed. Ex taken below
+    return 0;
+}
+
+
+#include<stdio.h>
+void fun(int a){
+    a = 34;
+    return;
+}
+
+// Here values will not be changed as above void block has different a variable in different 
+// memory in the ram so void and main both the blocks are different.
+int main(){
+    int a = 5;
+    printf("%d\n",a);
+    fun(a);
+    printf("%d",a);
+    return 0;
+}
+
+// Similarly
+
+#include<stdio.h>
+void fun(int* a){
+    *a = 34;
+    return;
+}
+
+// Here values will be changed as we used pointers where values will be changed in the same address.
+int main(){
+    int a = 5;
+    printf("%d\n",a);
+    fun(&a);
+    printf("%d",a);
     return 0;
 }
