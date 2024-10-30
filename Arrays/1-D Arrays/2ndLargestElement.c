fbc6d57edebca5118b6011dd1a77949b23e0e550
VickyKumarOfficial <vickykofficial890@gmail.com>
2024-10-30 12:23:39 +0530

    2nd max

--- Arrays/1-D Arrays/2ndLargestElement.c
+++ Arrays/1-D Arrays/2ndLargestElement.c
@@ -1,23 +1,24 @@
 // Find the 2nd largest number/element from the array.
 
 #include<stdio.h>
+#include<limits.h>
 int main(){
-    int n; printf("Size of the array :"); scanf("%d", &n);
-    int a[n];
-    for (int i = 0; i < n; i++) {
-        printf("Enter the element %d in the array :", i+1);
-        scanf("%d", &a[n]);
-    } int max = a[0];
-    for (int i = 0; i < n; i++) {
-        if(a[0]>a[i]) {
-            max = a[0];
-            printf("The largest number is = %d", max);
-            if(max>a[i]){
-                max = a[0];
-                printf("The 2nd largest number is = %d", max);
-            }
-        }
-    
+    // int n; printf("Size of the array :"); scanf("%d", &n);
+    // int a[n];
+    // for (int i = 0; i < n; i++) {
+    //     printf("Enter element no. %d of the array :",i+1);
+    //     scanf("%d", &a[i]);
+    // }
+    int a[6] = {1, 2, 3, 4, 5, 6};
+    int max = INT_MIN, smax = INT_MIN;
+    for (int i = 0; i < 6; i++) {
+        if (max < a[i]) max = a[i];
     }
+    printf("The largest element = %d\n",max);
+    for (int i = 0; i < 6; i++) {
+        if (smax < a[i] && a[i] != max) smax = a[i];
+    }
+    printf("Second largest number = %d",smax);
+    
     return 0;
 }
