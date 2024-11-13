f98f13a6c7cd18396384b950bb4a2305e8820439
VickyKumarOfficial <vickykofficial890@gmail.com>
2024-11-13 08:41:50 +0530

    update

---
+++ Pattern/Nested Loops/Square Patterns/Squares/AlphabaticSquare.c
@@ -0,0 +1,23 @@
+// Display the folowing pattern:
+/*  A B C
+    A B C
+    A B C
+*/
+
+// #include<stdio.h>
+
+// int main(){
+//     int n;
+//     printf("Enter a number :");
+//     scanf("%d",&n);
+//     for (int i = 1; i <= n; i++){
+//         for (int j = 1; j <= n; j++){
+//             printf("%c ",j+64); // j is initialised by 1. So, frist time 1 + 64 = 65.
+//                                 // 65 is A and however j++ is getting increment so value 
+//                                 // will increase 1 by 1 and hence A B C D.. will be printed till the condition is true. 
+//         }
+//         printf("\n");
+//     }
+    
+//     return 0;
+// }
--- Pattern/Nested Loops/Square Patterns/Squares/NumberDecrement.c
+++ Pattern/Nested Loops/Square Patterns/Squares/NumberDecrement.c
@@ -3,12 +3,13 @@
 // 3 3 3 3
 // 2 2 2 2
 // 1 1 1 1
+
 #include<stdio.h>
 
 int main(){
     int n; printf("Enter a number :"); scanf("%d", &n);
-    for (int i = n; i <= 1; i++) {
-        for (int j = 1; j <= i; j++) {
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= n; j++) {
             printf("%d ",i);
         }
         printf("\n");
@@ -16,20 +17,3 @@ int main(){
     
     return 0;
 }
-// #include<stdio.h>
-
-// int main(){
-//     int n;
-//     printf("Enter a number :");
-//     scanf("%d",&n);
-//     for (int i = n; i >= 1; i--)
-//     {
-//         for (int j = 1; j <= n; j++){
-            
-//             printf("%d ",i);
-//         }
-//         printf("\n");
-//     }
-    
-//     return 0;
-// }
