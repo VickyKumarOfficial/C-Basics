64ad4b3a68bb64b308084a34116c66d58aeda3f9
VickyKumarOfficial <vickykofficial890@gmail.com>
2025-01-12 16:35:32 +0530

    Bubble sort code

--- Sorting/Bubble.c
+++ Sorting/Bubble.c
@@ -1,7 +1,18 @@
+// Code for bubble sort.
+
 #include<stdio.h>
 
-void bubble(int a, int n) {
-    
+void bubble_sort(int a[], int n) {
+    // int temp;
+    for (int i = 1; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if(a[j] > a[j+1]) {
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
 }
 
 int main(){
@@ -10,8 +21,8 @@ int main(){
     scanf("%d", &n);
     int a[n];
     printf("Enter the elements in the array:\n");
-    for (int i = 0; i < n; i++) scsanf("%d", &a[i]);
-    bubble(a, n);
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+    bubble_sort(a, n);
     printf("Your sorted array is : ");
     for (int i = 0; i < n; i++) printf("%3d", a[i]);
     return 0;
